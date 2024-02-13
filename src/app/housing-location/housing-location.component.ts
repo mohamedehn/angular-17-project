2a93b1d78df8b438116cf336dae3ac2e0f28c635
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2024-02-14 00:36:54 +0100

    feat assets and interface

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocation } from "../housing-location";
 
 @Component({
-  selector: 'app-housing-location',
+  selector: "app-housing-location",
   standalone: true,
   imports: [CommonModule],
   template: `
-
+    <section class="listing">
+      <img
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        class="listing-photo"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
   `,
-  styleUrls: ['./housing-location.component.scss']
+  styleUrls: ["./housing-location.component.scss"],
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
